bddcea59799f84c97b1849ca83d65e2201539871
Florsha <floramayjoym@gmail.com>
2021-06-22 14:42:12 +0800

    PushUpdates

--- FPDF/print/report.php
+++ FPDF/print/report.php
@@ -274,14 +274,20 @@ foreach($expenses as $expense){
         $items = queryItem("call normal_tranche_region('$expense->id')");
 //
 //      if(count($items)>0)
+
         if(!($expense->id == 16 || $expense->id == 17 || $expense->id == 18 || $expense->id == 19 || $expense->id == 45 || $expense->id == 44 || $expense->id == 42  || $expense->id == 32  || $expense->id == 5 ))
             $pdf->displayExpense($expense->description); //display expense if no value from first
+        else
+            $pdf->displayExpense($expense->description);
+
+
 
         foreach($items as $item){
 
             if(empty($item->description) && ($item->expense_id == 16 || $item->expense_id == 17 || $item->expense_id == 18 || $item->expense_id == 19 || $item->expense_id == 45 || $item->expense_id == 44 || $item->expense_id == 42 || $item->expense_id == 32 || $item->expense_id == 5)){
-                $pdf->SetFont('Arial','b',7);
+                $pdf->SetFont('Arial','',7);
                 $item->description = $expense->description;
+
             } else {
                 $pdf->SetFont('Arial','',7);
             }
